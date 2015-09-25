71bcd76eb2981a9c71657de8d2eae647aaa08fe4
rikanulo <markezhkov@gmail.com>
2015-09-25 12:37:46 +0300

    index paths fixed

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 module.exports = {
-  ATSDClient: require('lib/client').ATSDClient,
+  ATSDClient: require('./lib/client').ATSDClient,
 
-  Entities: require('lib/entities').Entities,
-  Metrics: require('lib/metrics').Metrics,
+  Entities: require('./lib/entities').Entities,
+  Metrics: require('./lib/metrics').Metrics,
 
-  Properties: require('lib/properties').Properties,
-  Alerts: require('lib/alerts').Alerts,
+  Properties: require('./lib/properties').Properties,
+  Alerts: require('./lib/alerts').Alerts,
 
-  Series: require('lib/series').Series
+  Series: require('./lib/series').Series
 };
