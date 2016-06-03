2edbcb96fb17e37a67b076c6bbf6ea0e079cba1b
shmgrinsky <unrealwork@gmail.com>
2016-06-03 10:03:44 +0300

    Squashed commit of the following:
    commit a93886c5c0cf69f6454963ca02a9b410625f1a04
    Author: shmgrinsky <unrealwork@gmail.com>
    Date:   Fri Jun 3 10:02:52 2016 +0300
    
        get method migrate to new api
    
    commit 5e250e1484a6cd313244628f481af2bd3dfbde38
    Author: shmgrinsky <unrealwork@gmail.com>
    Date:   Wed Jun 1 17:44:46 2016 +0300
    
        get method migrate to new api

---
+++ src/tests/series-test.js
@@ -0,0 +1,37 @@
+var chai = require('chai');
+var expect = chai.expect; // we are using the "expect" style of Chai
+var Series = require('../../lib/series').Series;
+
+
+/**
+ * @test test base query
+ */
+
+describe('SeriesGetTest', function () {
+
+    it('test series get query', function () {
+        var series = new Series(options);
+        var query = {
+            "startDate": "2016-02-22T13:37:00Z",
+            "endDate": "2016-02-22T13:40:00Z",
+            "entity": "nurswgvml007",
+            "metric": "mpstat.cpu_busy"
+        };
+        var expected_response = [
+            {
+                "entity": "nurswgvml007",
+                "metric": "mpstat.cpu_busy",
+                "tags": {},
+                "type": "HISTORY",
+                "aggregate": {
+                    "type": "DETAIL"
+                },
+                "data": []
+            }
+        ];
+        series.get(query, function (error, response, data) {
+            console.log(data);
+            expect(data).to.deep.equal(expected_response);
+        });
+    });
+});
