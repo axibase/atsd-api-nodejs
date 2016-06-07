f27a2fce9b68e770adba7ebb5f9cf8603dd1c290
shmgrinsky <unrealwork@gmail.com>
2016-06-07 17:47:48 +0300

    properties api fixed

--- src/lib/properties.js
+++ src/lib/properties.js
@@ -1,4 +1,9 @@
 'use strict';
+
+/**
+ * @author Igor Shmagrinskiy
+ */
+
 var util = require('util');
 
 var ATSDClient = require('./client').ATSDClient;
@@ -7,54 +12,82 @@ var Properties = exports.Properties = function(options) {
     ATSDClient.call(this, options);
 };
 
+var propertiesPath = 'properties';
 util.inherits(Properties, ATSDClient);
 
-// Properties: Query
-// https://axibase.com/atsd/api/#properties:-query
-Properties.prototype.get = function(payload, callback) {
-    var path = 'properties';
+/**
+ * Retrieve property records matching specified filters.
+ * {@link https://github.com/axibase/atsd-docs/blob/master/api/data/properties/query.md Properties Query}
+ *
+ * @param {Object} payload - request body
+ * @param {Function} callback - result function
+ */
+Properties.prototype.query = function(payload, callback) {
+    var path = propertiesPath + '/query';
 
     this.postRequest(path, {}, payload, function(error, response, body) {
         callback(error, response, body);
     });
 };
 
-// Properties: Query for Entity and Type
-// https://axibase.com/atsd/api/#properties:-query-for-entity-and-type
-Properties.prototype.getByEntityAndType = function(entity, type, params, callback) {
-    var path = 'properties/' + entity + '/types/' + type;
+/**
+ *  Returns an array of property types for the entity.
+ *  {@link https://github.com/axibase/atsd-docs/blob/master/api/data/properties/type-query.md Type Query}
+ *
+ * @param {String} entity - name of entity
+ * @param {Function} callback - result function
+ */
 
-    this.getRequest(path, params, {}, function(error, response, body) {
+Properties.prototype.getTypesByEntity = function(entity, callback) {
+    var path = propertiesPath + entity + '/types';
+
+    this.getRequest(path, {}, {}, function(error, response, body) {
         callback(error, response, body);
     });
 };
 
-// Properties: Property Types
-// https://axibase.com/atsd/api/#properties:-property-types
-Properties.prototype.getPropertyTypes = function(entity, params, callback) {
+/**
+ *  Returns an array of property types for the entity.
+ *  {@link https://github.com/axibase/atsd-docs/blob/master/api/data/properties/type-query.md Type Query}
+ *
+ * @param {String} entity - name of entity
+ * @param {Function} callback - result function
+ */
+
+Properties.prototype.getPropertyTypes = function(entity, callback) {
     var path = 'properties/' + entity + '/types';
 
-    this.getRequest(path, params, {}, function(error, response, body) {
+    this.getRequest(path, {}, {}, function(error, response, body) {
         callback(error, response, body);
     });
 };
 
-// Properties: Insert
-// https://axibase.com/atsd/api/#properties:-insert
+/**
+ * Insert an array of properties.
+ * {@link https://github.com/axibase/atsd-docs/blob/master/api/data/properties/insert.md Insert}
+ *
+ * @param {Object} payload - body of the request
+ * @param {Function} callback - result function
+ */
 Properties.prototype.insert = function(payload, callback) {
-    var path = 'properties/insert';
+    var path = propertiesPath + '/insert';
 
     this.postRequest(path, {}, payload, function(error, response, body) {
         callback(error, response, body);
     });
 };
 
-// Properties: Batch
-// https://axibase.com/atsd/api/#properties:-batch
-Properties.prototype.batch = function(payload, callback) {
-    var path = 'properties';
+/**
+ * Delete property records that match specified filters.
+ * {@link https://github.com/axibase/atsd-docs/blob/master/api/data/properties/delete.md Delete}
+ *
+ * @param {Object} payload
+ * @param {Function} callback
+ */
+Properties.prototype.delete = function(payload, callback) {
+    var path = propertiesPath + '/delete';
 
-    this.patchRequest(path, {}, payload, function(error, response, body) {
+    this.postRequest(path, {}, payload, function(error, response, body) {
         callback(error, response, body);
     });
 };
---
+++ src/tests/properties-test.js
@@ -0,0 +1,70 @@
+'use strict';
+
+/**
+ * @author Igor Shmagrinskiy <unrealwork@gmail.com>
+ */
+
+var chai = require('chai');
+var expect = chai.expect; // we are using the 'expect' style of Chai
+var Properties = require('../lib/properties').Properties;
+var fs = require('fs');
+var testOptionsPath = __dirname + '/test-options.json';
+var options = JSON.parse(fs.readFileSync(testOptionsPath, 'utf8'));
+var testDataQueryPath = __dirname + '/data/properties';
+var properties = new Properties(options);
+
+describe('Properties Test', function() {
+
+    function correctProperty(property) {
+        var correct = true;
+        correct = correct && typeof(property === 'object');
+        correct = correct && typeof(property.type === 'string');
+        correct = correct && typeof(property.entity === 'string');
+        correct = correct && typeof(property.key === 'object');
+        correct = correct && typeof(property.tags === 'object');
+        correct = correct && typeof(property === 'string');
+        return correct;
+    }
+
+    function correctProperties(propertyList) {
+        for (var i = 0; i < propertyList.length; i++) {
+            if (!correctProperty(propertyList[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function correctResult(error, response, properties) {
+        return !error &&
+            response.statusCode === 200 &&
+            correctProperties(properties);
+    }
+
+    it('query', function(done) {
+        var payload = JSON.parse(fs.readFileSync(testDataQueryPath + '/query/query.json'));
+
+        properties.query(payload, function(error, response, properties) {
+            expect(correctResult(error, response, properties)).to.equal(true);
+            done();
+        });
+    });
+
+    it('insert', function(done) {
+        var payload = JSON.parse(fs.readFileSync(testDataQueryPath + '/insert/insert.json'));
+
+        properties.insert(payload, function(error, response) {
+            expect(response.statusCode === 200).to.equal(true);
+            done();
+        });
+    });
+
+    it('delete', function(done) {
+        var payload = JSON.parse(fs.readFileSync(testDataQueryPath + '/delete/delete.json'));
+
+        properties.delete(payload, function(error, response) {
+            expect(response.statusCode === 200).to.equal(true);
+            done();
+        });
+    });
+});
